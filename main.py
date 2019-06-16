cbe55a103b68326973c3ec0b1524fe27a7349b96
nath54 <cerisara.nathan@gmail.com>
2019-06-16 17:25:55 +0100

    a

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import random,pygame,time
 from pygame.locals import *
 
-pygmae.init()
+pygame.init()
 
 btex,btey=700,500
 tex,tey=700,500
@@ -11,11 +11,16 @@ fenetre=pygame.display.set_mode([tex,tey])
 pygame.display.set_caption("runner")
 pygame.key.set_repeat(40,30)
 
-
 def rx(x): return int(x/btex*tex)
 def ry(y): return int(y/btey*tey)
 
-panims=[]
+font=pygame.font.SysFont("Serif",ry(15))
+
+dimgs="images/"
+panims=[["p1-1.png","p1-2.png","p1-3.png","p1-4.png","p1-5.png","p1-6.png","p1-7.png""p1-8.png"]]
+
+imgsbackgrounds=[]
+imgbg=pygame.transform.scale(pygame.image.load(random.choice(imgsbackgrounds)),[tex*2,tey])
 
 class Perso:
     def __init__(self,nom,tpi,ks):
@@ -31,11 +36,17 @@ class Perso:
         self.db=time.time()
         self.tb=0.01
         self.imgs=[]
+        for i in panims[tpi]: imgs.append( pygame.transform.scale(pygame.image.load(dimgs+i),[self.tx,self.ty])
         self.an=0
         self.image=self.imgs[self.an]
         self.dan=time.time()
         self.tan=0.1
         self.vie=3
+        self.tpinv=0
+        self.dinv=time.time()
+        self.dclign=time.time()
+        self.tclign=0.2
+        self.isclign=False
     def bouger(self,aa):
         if time.time()-self.db >= self.tb:
             self.db=time.time()
@@ -56,6 +67,11 @@ class Perso:
             self.dan=time.time()
             self.an+=1
             if self.an>=len(self.imgs): self.an=0
+            self.image=self.imgs[self.an]
+        if tpinv>0:
+            if time.time()-self.dclign >= self.tclign:
+                self.dclign=time.time()
+                self.isclign=not isclign
 
 def verif_keys(persos):
     keys=pygame.key.get_pressed()
@@ -65,15 +81,22 @@ def verif_keys(persos):
         if keys[p.keyleft]:  p.bouger("left")
         if keys[p.keyright]:  p.bouger("right")
     
-def aff(persos,obstacles):
+def aff(persos,obstacles,fps):
+    fenetre.fill((0,0,0))
     for p in persos:
-        
-    
+        if p.tpinv <= 0: fenetre.blit(p.img,[p.px,p.py])
+        else:
+            if not p.isclign: fenetre.blit(p.img,[p.px,p.py])
+        fenetre.blit(font.render(p.nom,20,p.cl),[p.px,p.py-ry(15)])
+    fenetre.blit(font.render("fps : "+fps,20,(255,255,255)),[rx(15),ry(15)])
+    pygame.display.update()
 
 obstacles=[]
 persos=[]
+fps=0
 encour=True
 while encour:
+    t1=time.time()
     for p in persos: p.anim()
     verif_keys(persos)
     aff(persos,obstacles)
@@ -81,5 +104,7 @@ while encour:
         if event.type==QUIT: exit()
         elif event.type==KEYDOWN:
             if event.key==K_ESCAPE: encour=False
+    t2=time.time()
+    fps=str(int(1./(t2-t1)))
             
 
